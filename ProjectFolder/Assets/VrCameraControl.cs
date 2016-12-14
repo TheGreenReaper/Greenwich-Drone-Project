2bf2705fc5121e4fad00a70cca1ddcca7437013a
TheGreenReaper <daniel.lalov@gmail.com>
2016-12-14 14:37:37 +0000

    v1.2
    - Added proper camera
    - Added simple way to navigate around the scene
    - Added skybox....for some reason

---
+++ ProjectFolder/Assets/VrCameraControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrCameraControl : MonoBehaviour {
+    public Transform camera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            transform.Translate(camera.forward);
+        }
+        //if (Input.GetButton("Tap"))
+        //{
+        //    transform.Translate(camera.forward * Time.deltaTime);
+        //}
+	}
+}
