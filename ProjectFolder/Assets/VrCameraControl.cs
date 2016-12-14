6b138aabe3c6f7e7f752a93517e837c6f4c1a267
TheGreenReaper <daniel.lalov@gmail.com>
2016-12-14 16:19:42 +0000

    v1.3
    Minor alterations

--- ProjectFolder/Assets/VrCameraControl.cs
+++ ProjectFolder/Assets/VrCameraControl.cs
@@ -1,23 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class VrCameraControl : MonoBehaviour {
+public class VrCameraControl : MonoBehaviour
+{
     public Transform camera;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public Material mat1;
+    public Material mat2;
+    public Text input1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetMouseButton(0))
         {
             transform.Translate(camera.forward);
         }
-        //if (Input.GetButton("Tap"))
+        input1.text = "Mouse X: " + Input.GetAxis("Mouse X").ToString("F1") + "\n " +
+            "Mouse Y: " + Input.GetAxis("Mouse X").ToString("F1");
+
+
+
+
+
+        //if (Input.GetAxis("6") != 0)
         //{
-        //    transform.Translate(camera.forward * Time.deltaTime);
+        //    SetMat();
         //}
-	}
+        //if (Input.GetAxis("Mouse Y") != null)
+        //{
+        //    SetMat();
+        //}
+    }
+    void SetMat()
+    {
+        if (RenderSettings.skybox.name == mat1.name)
+        {
+            RenderSettings.skybox = mat2;
+        }
+        else
+        {
+            RenderSettings.skybox = mat1;
+        }
+    }
 }
