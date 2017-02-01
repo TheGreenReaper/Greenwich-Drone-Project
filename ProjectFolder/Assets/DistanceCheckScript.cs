aa1bef6810e5031acad38bc04d746ab670173c4f
TheGreenReaper <daniel.lalov@gmail.com>
2017-02-01 14:52:07 +0000

    v1.21
    - stable build ~8 min playable time before warning displayed

---
+++ ProjectFolder/Assets/DistanceCheckScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceCheckScript : MonoBehaviour {
+    public Transform player;
+    float distance = 150;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(Vector3.Distance(player.position, transform.position));
+        if (Vector3.Distance(player.position, transform.position) < distance)
+        {
+            GetComponent<MeshRenderer>().enabled = true;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+        }
+	}
+}
